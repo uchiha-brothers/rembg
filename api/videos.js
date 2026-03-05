f702aeaea214f52ceb6fde73cd8b2212d9ebaefe
uchiha-brothers <159233237+uchiha-brothers@users.noreply.github.com>
2026-03-06 01:59:01 +0530

    Update videos.js

--- api/videos.js
+++ api/videos.js
@@ -1,15 +1,8 @@
 import { createClient } from "@supabase/supabase-js"
-import formidable from "formidable"
-import fs from "fs"
-import FormData from "form-data"
-
-export const config = {
-  api: { bodyParser: false }
-}
 
 const supabase = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_KEY
+process.env.SUPABASE_URL,
+process.env.SUPABASE_KEY
 )
 
 export default async function handler(req,res){
@@ -27,14 +20,9 @@ return res.json(data)
 
 if(req.method==="POST"){
 
-const form = formidable()
+const {title,url} = req.body
 
-form.parse(req, async (err,fields,files)=>{
-
-const title = Array.isArray(fields.title) ? fields.title[0] : fields.title
-const file = Array.isArray(files.video) ? files.video[0] : files.video
-
-if(!title || !file){
+if(!title || !url){
 return res.status(400).json({error:"Missing data"})
 }
 
@@ -47,26 +35,12 @@ if(existing.length){
 return res.status(400).json({error:"Title exists"})
 }
 
-const formData = new FormData()
-
-formData.append("reqtype","fileupload")
-formData.append("fileToUpload",fs.createReadStream(file.filepath))
-
-const upload = await fetch("https://catbox.moe/user/api.php",{
-method:"POST",
-body:formData
-})
-
-const url = (await upload.text()).trim()
-
 await supabase.from("videos").insert({
 title:title,
 url:url
 })
 
-res.json({success:true,url})
-
-})
+res.json({success:true})
 
 }
 
